b1d729e7d980d1d12d4765da1e957afbfb15507b
Abhay Chouhan <abhaydinakar246@gmail.com>
2026-03-13 19:12:05 +0530

    deployment setup

--- app.py
+++ app.py
@@ -673,3 +673,9 @@ if __name__ == '__main__':
     print("Starting Tax Invoice Application...")
     print("Open http://127.0.0.1:5000 in your browser")
     app.run(debug=True, port=5000)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
